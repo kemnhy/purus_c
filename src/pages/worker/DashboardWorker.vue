90f2a83df8bf67454fe037a0de0d193a2a5eda26
miyaajd <ehdal1032@gmail.com>
2025-11-17 17:45:07 +0900

    notice / login in progress

--- src/pages/worker/DashboardWorker.vue
+++ src/pages/worker/DashboardWorker.vue
@@ -2,30 +2,38 @@
   <div class="max-w-md m-auto">
     <!-- 헤더 -->
     <header
-      :class="$route.path !== '/worker/dashboard/home' ? 'bg-white' : 'bg-[#f0faff]'"
-      class="bg-[#f0faff] h-15 border-b-white flex px-10 justify-between items-center">
+      :class="
+        $route.path !== '/worker/dashboard/home' ? 'bg-white' : 'bg-[#f0faff]'
+      "
+      class="bg-[#f0faff] h-15 border-b-white flex px-10 justify-between items-center"
+    >
       <!-- 뒤로가기 -->
       <div>
         <button @click="goBack" v-if="$route.path !== '/worker/dashboard/home'">
           <!-- 홈이 아닐때만 보이기 -->
-          <i class="fa-solid fa-angle-left text-xl text-[#092857]"></i>
+          <i class="fa-solid fa-angle-left text-xl text-[#092857] cursor-pointer"></i>
         </button>
       </div>
       <!-- 알림 아이콘 -->
-      <div @click="openNotice" class="relative">
-        <i class="fa-solid fa-bell text-[#092857] text-xl"></i>
-        <span v-if="notice" class="w-3 h-3 bg-[#F12929] absolute rounded-full text-white">
+      <div @click="openNotice = true" class="relative">
+        <i class="fa-solid fa-bell text-[#092857] text-xl cursor-pointer"></i>
+        <span
+          v-if="notice"
+          class="w-3 h-3 bg-[#F12929] absolute rounded-full text-white"
+        >
           {{}}
         </span>
       </div>
     </header>
-    <WorkerNotice />
+    <WorkerNotice v-if="openNotice" @close="openNotice = false" />
     <!-- 메인 > 라우터 불러옴 -->
     <main>
       <router-view></router-view>
     </main>
     <!-- 하단고정메뉴 -->
-    <nav class="fixed bottom-0 left-1/2 -translate-x-1/2 w-full max-w-md bg-[#f0faff] h-17 z-10">
+    <nav
+      class="fixed bottom-0 left-1/2 -translate-x-1/2 w-full max-w-md bg-[#f0faff] h-17 z-10"
+    >
       <div class="h-full grid grid-cols-3">
         <!-- 이달의 청소-->
         <router-link
@@ -35,7 +43,8 @@
               ? 'text-[#296af1] dark:text-blue-400'
               : 'text-[#888888] dark:text-gray-400'
           "
-          :to="{ name: 'Month' }">
+          :to="{ name: 'Month' }"
+        >
           <i class="fa-solid fa-calendar"></i>
         </router-link>
         <!-- 홈 -->
@@ -46,7 +55,8 @@
               ? 'text-[#296af1] dark:text-blue-400'
               : 'text-[#888888] dark:text-gray-400'
           "
-          :to="{ name: 'WorkerHome' }">
+          :to="{ name: 'WorkerHome' }"
+        >
           <i class="fa-solid fa-house"></i>
         </router-link>
         <!-- 마이페이지 -->
@@ -57,7 +67,8 @@
               ? 'text-[#296af1] dark:text-blue-400'
               : 'text-[#888888] dark:text-gray-400'
           "
-          :to="{ name: 'MyPage' }">
+          :to="{ name: 'MyPage' }"
+        >
           <i class="fa-solid fa-user"></i>
         </router-link>
       </div>
@@ -67,6 +78,7 @@
 
 <script setup>
 import WorkerNotice from "@/components/worker/WorkerNotice.vue";
+import { ref } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
@@ -75,4 +87,7 @@ const router = useRouter();
 const goBack = () => {
   router.back();
 };
+
+// 알림창 열기
+const openNotice = ref(false);
 </script>
